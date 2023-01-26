3cb27b0e3fddc190cfc0c7d54af3bd50563790e9
semin <semin8890@gmail.com>
2023-01-26 11:35:15 +0900

    뒤로 가기 처리 추가

--- app/src/main/java/kr/kro/fatcats/allerview/ui/activity/MainActivity.kt
+++ app/src/main/java/kr/kro/fatcats/allerview/ui/activity/MainActivity.kt
@@ -2,22 +2,33 @@ package kr.kro.fatcats.allerview.ui.activity
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kr.kro.fatcats.allerview.R
+import kr.kro.fatcats.allerview.ui.fragment.CameraFragment
+import kr.kro.fatcats.allerview.util.LogUtil
 import kr.kro.fatcats.allerview.viewmodel.MainViewModel
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
     private val mainViewModel : MainViewModel by viewModels()
+    private val nav : NavController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+    override fun onBackPressed() {
+        val fragmentLabel = nav.currentDestination?.label
+        if(fragmentLabel == CameraFragment().javaClass.simpleName){
+            finish()
+        }else{
+            super.onBackPressed()
+        }
     }
 }
