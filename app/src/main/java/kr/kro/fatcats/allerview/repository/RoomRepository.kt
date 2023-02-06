134ae20227f68c49053f9e0ba3709f0953972698
semin <semin8890@gmail.com>
2023-02-06 22:58:48 +0900

    RoomRepository 미사용 코드 삭제

--- app/src/main/java/kr/kro/fatcats/allerview/repository/RoomRepository.kt
+++ app/src/main/java/kr/kro/fatcats/allerview/repository/RoomRepository.kt
@@ -2,26 +2,16 @@ package kr.kro.fatcats.allerview.repository
 
 import kotlinx.coroutines.*
 import kr.kro.fatcats.allerview.di.annotations.IoDispatcher
-import kr.kro.fatcats.allerview.model.barcodeInfo.BarcodeInfo
 import kr.kro.fatcats.allerview.model.local.room.AppDataBase
 import kr.kro.fatcats.allerview.model.local.room.entity.Food
 import javax.inject.Inject
 
 class RoomRepository @Inject constructor(
     db : AppDataBase,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
-    private val externalScope: CoroutineScope,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
 
     private val dao = db.foodDao()
-
-//    suspend fun importToBadrcode(barcode : Long) : Food?{
-//        var data : Food? = null
-//        externalScope.launch(ioDispatcher) {
-//            data = dao.loadByBarcode(barcode)
-//        }
-//        return data
-//    }
 
     suspend fun importToBarcode(barcode : Long) : Food? = withContext(ioDispatcher) {
         dao.loadByBarcode(barcode)
