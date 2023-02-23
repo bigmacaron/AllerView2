24a2282e54bebd95b870f51c1e7df83b5c40e897
hyeonseok <startwo09@gmail.com>
2023-02-23 10:15:14 +0900

    Feat: 커스텀 버튼 추가

--- app/src/main/java/kr/kro/fatcats/allerview/ui/widget/AVButton.kt
+++ app/src/main/java/kr/kro/fatcats/allerview/ui/widget/AVButton.kt
@@ -1,24 +1,71 @@
 package kr.kro.fatcats.allerview.ui.widget
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
-import androidx.appcompat.widget.AppCompatButton
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import kr.kro.fatcats.allerview.R
 
-class AVButton : AppCompatButton {
+class AVButton : ConstraintLayout {
 
-    constructor(context: Context) : super(context) {}
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        init(context, attrs)
+    private lateinit var backgroundView: ConstraintLayout
+    private lateinit var imageView: ImageView
+    private lateinit var textView: TextView
+
+    constructor(context: Context) : super(context) {
+        init()
     }
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+        getAttrs(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
     ) {
-        init(context, attrs)
+        init()
+        getAttrs(attrs, defStyleAttr)
     }
 
-    private fun init(context: Context, attrs: AttributeSet?){}
+    private fun init() {
+        val infService = Context.LAYOUT_INFLATER_SERVICE
+        val li = context.getSystemService(infService) as LayoutInflater
+        val v = li.inflate(R.layout.alerview_button, this, false)
+        addView(v)
 
+        backgroundView = findViewById(R.id.backgroundLayout)
+        imageView = findViewById(R.id.ivIcon)
+        textView = findViewById(R.id.tvTitle)
+    }
+
+    private fun getAttrs(attrs: AttributeSet) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.AVButton)
+        setAttrs(typedArray)
+    }
+
+    private fun getAttrs(attrs: AttributeSet, defStyle: Int) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.AVButton, defStyle, 0)
+        setAttrs(typedArray)
+    }
+
+    private fun setAttrs(typedArray: TypedArray) {
+        val backgroundResId = typedArray.getResourceId(R.styleable.AVButton_backgroundTint, R.color.dark_primary_85)
+        val symbolResId = typedArray.getResourceId(R.styleable.AVButton_symbol, R.drawable.ic_launcher_foreground)
+        val textColorResId = typedArray.getColor(R.styleable.AVButton_textColor, ContextCompat.getColor(context, R.color.black))
+        val textResId = typedArray.getString(R.styleable.AVButton_text)
+
+//        backgroundView.backgroundTintList = ContextCompat.getColorStateList(context, backgroundResId)
+        imageView.setImageResource(symbolResId)
+        textView.setTextColor(textColorResId)
+        textView.text = textResId
+
+        typedArray.recycle()
+    }
 
 }
