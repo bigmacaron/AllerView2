8a965bab0d9695d9887fbf043919d01a94b2169e
hyeonseok <startwo09@gmail.com>
2023-08-07 17:08:36 +0900

    [오현석] Feat: 검색 결과 화면 앱바 변경 리스너 등록

--- app/src/main/java/kr/kro/fatcats/allerview/ui/fragment/BarcodeResultFragment.kt
+++ app/src/main/java/kr/kro/fatcats/allerview/ui/fragment/BarcodeResultFragment.kt
@@ -1,6 +1,5 @@
 package kr.kro.fatcats.allerview.ui.fragment
 
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,16 +8,37 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.appbar.AppBarLayout
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kr.kro.fatcats.allerview.R
 import kr.kro.fatcats.allerview.databinding.FragmentBarcodeResultBinding
 import kr.kro.fatcats.allerview.ui.activity.MainActivity
+import kr.kro.fatcats.allerview.ui.listener.AppBarStateChangeListener
 import kr.kro.fatcats.allerview.util.LogUtil
 import kr.kro.fatcats.allerview.viewmodel.MainViewModel
 
 
 class BarcodeResultFragment : BaseFragment<FragmentBarcodeResultBinding,MainViewModel>() {
+
+    private val appBarStateChangeListener: AppBarStateChangeListener =
+        object : AppBarStateChangeListener() {
+            override fun onStateChanged(appBarLayout: AppBarLayout, state: State) {
+                LogUtil.d(LogUtil.DEBUG_LEVEL_2,"onStateChanged state: $state")
+                when (state) {
+                    State.COLLAPSED -> {
+                        viewBinding?.tvBarcode?.run {
+                            visibility = View.VISIBLE
+                        }
+                    }
+                    else -> {
+                        viewBinding?.tvBarcode?.run {
+                            visibility = View.GONE
+                        }
+                    }
+                }
+            }
+        }
 
     override fun viewModel(): MainViewModel {
         val lazy: Lazy<MainViewModel> = viewModels(ownerProducer = { requireActivity() })
@@ -32,6 +52,7 @@ class BarcodeResultFragment : BaseFragment<FragmentBarcodeResultBinding,MainView
     override fun initData(viewBinding: FragmentBarcodeResultBinding) {
         viewBinding.viewModel = viewModel
         viewBinding.activity  = requireActivity() as MainActivity // Todo onBackPressed 이벤트를 어떻게 처리할지 추후에 수정..
+        viewBinding.appBar.addOnOffsetChangedListener(appBarStateChangeListener)
         subscriptionFoodInformation()
     }
 
